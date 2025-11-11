92d345e2afee10f80e364ff2b20c0ec00c35b0db
aMamaki <amamaki37@gmail.com>
2025-11-11 21:46:22 +0100

    Change server names to lowercase
    Updated server names to lowercase in listcfg.lua

--- listcfg.lua
+++ listcfg.lua
@@ -1,8 +1,8 @@
-Krystalmc:4.11.16:27.25r:Mevinggs
-ClearMc:4.11.16:15.25r:Mevinggs
-ClearMc2:4.11.16:15.25r:Mevinggs
-BOXHUNT:4.11.20:05.25r:sopelownyou
-BravoHvH:4.11.21:11.25r:Mevinggs
+krystalmc:4.11.16:27.25r:Mevinggs
+clearmc:4.11.16:15.25r:Mevinggs
+clearmc2:4.11.16:15.25r:Mevinggs
+hoxhunt:4.11.20:05.25r:sopelownyou
+bravohvh:4.11.21:11.25r:Mevinggs
 SzefyPl:5.11.07:49.25r:Kriper
 catpvp:5.11.?:?.25r:minhdayne_05
 krystalmcL:5.11.14:16.25r:Harf
