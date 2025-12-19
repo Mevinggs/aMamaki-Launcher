a5311c97e5bad41a852d6f9645f2753cacaa7c4b
Mevinggs <amamaki37@gmail.com>
2025-12-19 21:03:25 +0100

    Update Better Trees download link

--- dollandiaistneijerzadchcenasuciszyc.lua
+++ dollandiaistneijerzadchcenasuciszyc.lua
@@ -1,1 +1,1 @@
-https://github.com/Mevinggs/Veltragossa-client/releases/download/VELTRAGOSSZA/Better-Trees-1.9.1.6.jar
+https://github.com/Mevinggs/Veltragossa-client/releases/download/gg/Better-Trees-1.9.1.jar
